7a22d35f7b71a963b3f8f092d5aac7df7cd9b4a3
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2016-12-18 03:30:02 +0200

    feat(/parse;/dashboard): Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ro-diaspora-backend
+# ROdiaspora - backend
+
+# Instalare
+```
+git clone https://github.com/gov-ithub/ro-diaspora-backend.git
+cd ro-diaspora-backend
+cp .env_sample .env
+vim .env // editeaza credentialele cu ale tale
+npm install
+npm start
+```
+
+# Tehnologii folosite
+- Parse Server
+- Parse Dashboard
+- Express
---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
+var app = express();
+
+var serverURL = process.env.SERVER_URL || 'http://localhost:4040/parse';
+var appId = process.env.APP_ID || 'ROdiaspora';
+var masterKey = process.env.MASTER_KEY || 'ROdiaspora';
+
+var api = new ParseServer({
+  "databaseURI": process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
+  "cloud": __dirname + '/cloud/main.js',
+  "appId": appId,
+  "masterKey": masterKey,
+  "serverURL": serverURL,
+});
+
+var dashboard = new ParseDashboard({
+  "apps": [{
+    "appId": appId,
+    "masterKey": masterKey,
+    "serverURL": serverURL,
+    "appName": "ROdiaspora",
+    "iconName": "ROdiaspora.png",
+  }],
+  "iconsFolder": "icons"
+});
+
+app.use('/parse', api);
+app.use('/dashboard', dashboard);
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(4040);
